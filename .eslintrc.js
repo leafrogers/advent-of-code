94066e050f98aeaf3c0209f33383107e18a36c55
Keith Rogers <150157+keirog@users.noreply.github.com>
2020-04-08 12:22:06 +0100

    Days 1, 2, 3 (part 1)

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+	'comma-dangle': 0,
+	'indent': ['error', 'tab'],
+	'max-len': 0,
+	'no-console': 0,
+	'no-param-reassign': 0,
+	'no-restricted-syntax': 0,
+	'no-tabs': 0,
+	'no-use-before-define': 0
+  }
+};
